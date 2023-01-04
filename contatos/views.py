c17e5f727e5b21bc1c744713b3e4d5d53cb9eeb9
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2023-01-04 17:11:10 -0300

    Finaliza

--- contatos/views.py
+++ contatos/views.py
@@ -1,11 +1,11 @@
 from django.core.paginator import Paginator
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
 
 from .models import Contato
-
+from django.contrib import messages
 
 def index(request):
   contatos = Contato.objects.order_by('-id').filter(mostrar=True)
@@ -34,8 +34,14 @@ def busca(request):
   termo = request.GET.get('termo')
   campos = Concat('nome', Value(' '), 'sobrenome')
 
-  if (termo is None):
-    raise Http404()
+  if termo is None or not termo:
+    messages.add_message(
+      request, 
+      messages.ERROR, 
+      'Campo termo não pode ficar vazio'
+    )
+    
+    return redirect('index')
 
   contatos = Contato.objects.annotate(nome_completo=campos
   ).filter(
